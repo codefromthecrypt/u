21467b11168baa401412a137673d35d18687fa67
Adrian Cole <adrian@tetrate.io>
2021-12-27 09:05:16 +0800

    foo
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- dir_test.go
+++ dir_test.go
@@ -9,25 +9,22 @@ import (
 
 func TestDirError(t *testing.T) {
 	dir := t.TempDir()
-	pathToFile := path.Join(dir, "foo")
-	if os.WriteFile(pathToFile, []byte{}, 0600) != nil {
-		t.Fatalf("cannot write %s", pathToFile)
+	fileNotDir := path.Join(dir, "foo")
+	if os.WriteFile(fileNotDir, []byte{}, 0600) != nil {
+		t.Fatalf("cannot write %s", fileNotDir)
 	}
-	noteThisIsAFileNotADir, err := os.Open(pathToFile)
-	defer noteThisIsAFileNotADir.Close()
-
-	if err != nil {
-		t.Fatalf("cannot open %s", pathToFile)
-	}
-	_, err = noteThisIsAFileNotADir.ReadDir(1)
+	_, err := os.ReadDir(fileNotDir)
 	if err == nil {
-		t.Fatalf("wanted an error on os.File(%s).ReadDir()", pathToFile)
+		t.Fatalf("wanted an error on os.ReadDir(%s)", fileNotDir)
 	}
 	pErr, ok := err.(*os.PathError)
 	if !ok {
-		t.Fatalf("wanted an os.PathError on os.ReadDir(%s)", pathToFile)
+		t.Fatalf("wanted an os.PathError on os.ReadDir(%s)", fileNotDir)
+	}
+	if os.IsNotExist(err) {
+		t.Fatalf("didn't want os.IsNotExist() on os.ReadDir(%s)", fileNotDir)
 	}
 	if pErr.Err != syscall.ENOTDIR {
-		t.Fatalf("want syscall.ENOTDIR, have %s, on os.ReadDir(%s)", pErr.Err, pathToFile)
+		t.Fatalf("want syscall.ENOTDIR, have %s, on os.ReadDir(%s)", pErr.Err, fileNotDir)
 	}
 }
